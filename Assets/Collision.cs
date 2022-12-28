006fb62898e8e866670f9c3473742890d7251648
Ian Gagorik <ian.gagorik@gmail.com>
2022-12-28 10:19:10 -0800

    Add trigger logging

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -1,10 +1,14 @@
-using System;
 using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnCollisionEnter2D(Collision2D collision2D)
     {
-        Debug.Log($"{this.gameObject.name} collided with a {col.gameObject.name}");
+        Debug.Log($"{this.gameObject.name} collided with a {collision2D.gameObject.name}.");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        Debug.Log($"{this.gameObject.name} passed trough {collider2D.gameObject.name} trigger.");
     }
 }
