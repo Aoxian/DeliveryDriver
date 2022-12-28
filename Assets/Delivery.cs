6e8d8d0fc5022c4aafd7fa86ba0fb0302bd4b6ad
Ian Gagorik <ian.gagorik@gmail.com>
2022-12-28 15:57:05 -0800

    Some package delivery logic

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    private bool hasPackage = false;
     private void OnCollisionEnter2D(Collision2D collision2D)
     {
         Debug.Log($"{this.gameObject.name} collided with a {collision2D.gameObject.name}.");
@@ -9,7 +10,16 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
-        if (collider2D.tag == "Package") Debug.Log($"Picked up package: {collider2D.gameObject.name}");
-        if (collider2D.tag == "Customer") Debug.Log($"Visited the customer: {collider2D.gameObject.name}");
+        if (collider2D.tag == "Package" && !hasPackage)
+        {
+            hasPackage = true;
+            Debug.Log($"Picked up: {collider2D.gameObject.name}");
+        }
+
+        if (collider2D.tag == "Customer" && hasPackage)
+        {
+            hasPackage = false;
+            Debug.Log($"Package delivered to: {collider2D.gameObject.name}");
+        }
     }
 }
